cfd406ffa83390856f32792da481351967371a5c
Muhammad Taha <42276750+taha842@users.noreply.github.com>
2023-06-24 14:07:32 +0500

    ESP32 Modbus Tested.ino

--- ESP32 Modbus Tested.ino
+++ ESP32 Modbus Tested.ino
@@ -1,7 +1,5 @@
 /*
-This example reads 2 words (4 bytes) from address 52 of a server with id 1.
-address 52 = register 30053 (Eastron SDM630 Total system power)
-The ESP is connected to a max3485 with pins 17 (RX), 4 (TX) and 16 as RTS.
+The ESP is connected to a max485 with pins 17 (RX), 4 (TX) and 16 as RTS.
 */
 
 #include <Arduino.h>
